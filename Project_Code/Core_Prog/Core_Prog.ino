c7361e1f9e4dbb8a959a635d882ecf9bbf23a180
Nanopulse1 <s00177754@e1007-01.campus.itsligo.ie>
2018-02-22 17:31:41 +0000

    Motor_Servo_Update
    Servo now works, PIR does not work as of yet, need more research and
    time to look at examples

--- Project_Code/Core_Prog/Core_Prog.ino
+++ Project_Code/Core_Prog/Core_Prog.ino
@@ -2,34 +2,45 @@
 Servo myservo;
 int pos = 0;
 const int buttonPin = 6;
-int buttonState = 0; 
+int buttonState = 0;
+
+const int LED = 3;
+const int PIR = 7;
+int pirState = 0;
 
 void setup() {
   // put your setup code here, to run once:
 myservo.attach(2);
+
+pinMode(LED, OUTPUT);
+pinMode(PIR, INPUT);
+
 pinMode(buttonPin, INPUT);
 }
 
 void loop() {
 
   buttonState = digitalRead(buttonPin);
-  if (buttonState == LOW) {
-    
-    myservo.write(pos); 
-  } 
-  else if(buttonState == HIGH) {
-    for (pos = 0; pos <= 180; pos += 1) {
-    
+  if(buttonState == HIGH) {
+    for (pos = 0; pos <= 180; pos += 1) 
+    {
     myservo.write(pos);              
     delay(15);
-  
-   
-              }
-  }
-  else{
+    }
+                          }
+    else{
     pos=0;
-  }
-}
+        }
+
+        //LED
+         pirState = digitalRead(PIR);
+        if(pirState == LOW) {
+          digitalWrite(LED, HIGH); 
+                            }
+        else if(pirState == HIGH) {
+          digitalWrite(LED, LOW);
+        }
+            }
 
 void pinsInit()
 {
