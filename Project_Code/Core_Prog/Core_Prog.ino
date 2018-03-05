0838e7bf7f87630a36b31fd7e5ff3cdf5606d51e
Nanopulse1 <36006416+Nanopulse1@users.noreply.github.com>
2018-03-05 01:35:25 +0000

    Moment of Clarity
    Yes its too early to be coding in reality but realised how to start the servo from the HTML page, we use an empty pin as a toggle, turning it on and off from the page as shown in class  and read the value of the pin with an if statement to execute the code.

--- Project_Code/Core_Prog/Core_Prog.ino
+++ Project_Code/Core_Prog/Core_Prog.ino
@@ -4,6 +4,9 @@ int pos = 0;
 const int buttonPin = 6;
 int buttonState = 0;
 
+const int FUNCTION = 7;
+int functionState = 0;
+
 const int LED = 3;
 const int PIR = 7;
 int pirState = 0;
@@ -14,14 +17,15 @@ myservo.attach(2);
 
 pinMode(LED, OUTPUT);
 pinMode(PIR, INPUT);
-
+pinMode(FUNCTION, INPUT);
 pinMode(buttonPin, INPUT);
 }
 
 void loop() {
 
-  buttonState = digitalRead(buttonPin);
-  if(buttonState == HIGH) {
+  functionState = digitalRead(FUNCTION);
+
+  if(digitalRead(functionState == HIGH)){
     for (pos = 0; pos <= 180; pos += 1) 
     {
     myservo.write(pos);              
@@ -29,21 +33,10 @@ void loop() {
     }
                           }
     else{
-    pos=0;
+          pos=0;
         }
 
-        //LED
-         pirState = digitalRead(PIR);
-        if(pirState == LOW) {
-          digitalWrite(LED, HIGH); 
-                            }
-        else if(pirState == HIGH) {
-          digitalWrite(LED, LOW);
-        }
-            }
-
-void pinsInit()
-{
-}
+    functionState = LOW;
+  }
 
 
