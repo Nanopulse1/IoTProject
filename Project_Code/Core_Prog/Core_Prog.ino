6785c20ec480dda2426408cc9b1270b044ab4e5d
Nanopulse1 <s00177754@e1021-03.campus.itsligo.ie>
2018-03-05 10:38:44 +0000

    Bridge & REST Enabled
    Added code to use the bridge/REST functions in the project

--- Project_Code/Core_Prog/Core_Prog.ino
+++ Project_Code/Core_Prog/Core_Prog.ino
@@ -1,3 +1,4 @@
+/* V1
 #include <Servo.h>
 Servo myservo;
 int pos = 0;
@@ -19,24 +20,224 @@ pinMode(LED, OUTPUT);
 pinMode(PIR, INPUT);
 pinMode(FUNCTION, INPUT);
 pinMode(buttonPin, INPUT);
+functionState = 0;
+
 }
 
 void loop() {
 
   functionState = digitalRead(FUNCTION);
 
-  if(digitalRead(functionState == HIGH)){
+  if(functionState == digitalRead(HIGH)){
     for (pos = 0; pos <= 180; pos += 1) 
     {
     myservo.write(pos);              
     delay(15);
     }
-                          }
+    }
     else{
           pos=0;
         }
-
     functionState = LOW;
+ 
+    
   }
 
+V2 */
+#include <Bridge.h>
+#include <BridgeServer.h>
+#include <BridgeClient.h>
+#include <Servo.h>
 
+// Listen to the default port 5555, the Yún webserver
+// will forward there all the HTTP requests you send
+BridgeServer server;
+Servo myservo;
+
+int pos = 0;
+const int buttonPin = 6;
+int buttonState = 0;
+
+const int FUNCTION = 7;
+int functionState = 0;
+
+const int LED = 3;
+const int PIR = 7;
+int pirState = 0;
+
+void setup() {
+  // Bridge startup
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  Bridge.begin();
+  digitalWrite(13, HIGH);
+
+  myservo.attach(2);
+
+pinMode(LED, OUTPUT);
+digitalWrite(LED, LOW);
+pinMode(PIR, INPUT);
+pinMode(FUNCTION, INPUT);
+pinMode(buttonPin, INPUT);
+functionState = 0;
+
+  // Listen for incoming connection only from localhost
+  // (no one from the external network could connect)
+  
+  server.listenOnLocalhost();
+  server.begin();
+}
+
+void loop() {
+  // Get clients coming from server
+  BridgeClient client = server.accept();
+
+  // There is a new client?
+  if (client) {
+    // Process request
+    process(client);
+
+    // Close connection and free resources.
+    client.stop();
+  }
+
+  delay(50); // Poll every 50ms
+
+  functionState = digitalRead(FUNCTION);
+
+  if(functionState == digitalRead(HIGH)){
+    for (pos = 0; pos <= 180; pos += 1) 
+    {
+    myservo.write(pos);              
+    delay(15);
+    }
+    }
+    else{
+          pos=0;
+        }
+    functionState = LOW;
+}
+
+void process(BridgeClient client) {
+  // read the command
+  String command = client.readStringUntil('/');
+
+  // is "digital" command?
+  if (command == "digital") {
+    digitalCommand(client);
+  }
+
+  // is "analog" command?
+  if (command == "analog") {
+    analogCommand(client);
+  }
+
+  // is "mode" command?
+  if (command == "mode") {
+    modeCommand(client);
+  }
+}
+
+void digitalCommand(BridgeClient client) {
+  int pin, value;
+
+  // Read pin number
+  pin = client.parseInt();
+
+  // If the next character is a '/' it means we have an URL
+  // with a value like: "/digital/13/1"
+  if (client.read() == '/') {
+    value = client.parseInt();
+    digitalWrite(pin, value);
+  } else {
+    value = digitalRead(pin);
+  }
+
+  // Send feedback to client
+  client.print(F("Pin D"));
+  client.print(pin);
+  client.print(F(" set to "));
+  client.println(value);
+
+  // Update datastore key with the current pin value
+  String key = "D";
+  key += pin;
+  Bridge.put(key, String(value));
+}
+
+void analogCommand(BridgeClient client) {
+  int pin, value;
+
+  // Read pin number
+  pin = client.parseInt();
+
+  // If the next character is a '/' it means we have an URL
+  // with a value like: "/analog/5/120"
+  if (client.read() == '/') {
+    // Read value and execute command
+    value = client.parseInt();
+    analogWrite(pin, value);
+
+    // Send feedback to client
+    client.print(F("Pin D"));
+    client.print(pin);
+    client.print(F(" set to analog "));
+    client.println(value);
+
+    // Update datastore key with the current pin value
+    String key = "D";
+    key += pin;
+    Bridge.put(key, String(value));
+  } else {
+    // Read analog pin
+    value = analogRead(pin);
+
+    // Send feedback to client
+    client.print(F("Pin A"));
+    client.print(pin);
+    client.print(F(" reads analog "));
+    client.println(value);
+
+    // Update datastore key with the current pin value
+    String key = "A";
+    key += pin;
+    Bridge.put(key, String(value));
+  }
+}
+
+void modeCommand(BridgeClient client) {
+  int pin;
+
+  // Read pin number
+  pin = client.parseInt();
+
+  // If the next character is not a '/' we have a malformed URL
+  if (client.read() != '/') {
+    client.println(F("error"));
+    return;
+  }
+
+  String mode = client.readStringUntil('\r');
+
+  if (mode == "input") {
+    pinMode(pin, INPUT);
+    // Send feedback to client
+    client.print(F("Pin D"));
+    client.print(pin);
+    client.print(F(" configured as INPUT!"));
+    return;
+  }
+
+  if (mode == "output") {
+    pinMode(pin, OUTPUT);
+    // Send feedback to client
+    client.print(F("Pin D"));
+    client.print(pin);
+    client.print(F(" configured as OUTPUT!"));
+    return;
+  }
+
+  client.print(F("error: invalid mode "));
+  client.print(mode);
+}
+*/
